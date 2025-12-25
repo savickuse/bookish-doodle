d0b37b731cc4028e727e98e03206dfa3898d2fb2
savickuse <savickuse@users.noreply.github.com>
2025-12-26 01:29:26 +0200

    style: refactor module structure

--- src/api.js
+++ src/api.js
@@ -23,3 +23,30 @@ class API {
 
 // Update 10
 module.exports = API;
+
+
+// API module for DoodleNotes
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 20
+module.exports = API;
