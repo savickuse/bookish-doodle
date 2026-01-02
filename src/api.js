9d0793e42e6fc336634204a9b7e24ea0f427b556
savickuse <savickuse@users.noreply.github.com>
2026-01-02 02:34:24 +0200

    test(db): add input validation

--- src/api.js
+++ src/api.js
@@ -50,3 +50,30 @@ class API {
 
 // Update 20
 module.exports = API;
+
+
+// API module for DoodleNotes
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 31
+module.exports = API;
